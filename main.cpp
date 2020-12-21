c0c21f42028193c1a9efe940fdee7e167eebd06d
Juni <jwongso@users.noreply.github.com>
2020-12-21 21:15:49 +0100

    Update to use booleans instead of stacks

--- main.cpp
+++ main.cpp
@@ -1,47 +1,44 @@
 #include <iostream>
-#include <stack>
+#include <vector>
 
 using namespace std;
 
 int solution(const string& street) {
-    stack<char> houses;
-    stack<char> ground;
+    bool house(false);
+    bool space(false);
     int res = 0;
     unsigned int i = 0;
 
     while (i < street.size()) {
         if (street[i] == 'H') {
-            if (houses.empty()) {
-                houses.push(street[i]);
+            if (!house) {
+                house = true;
 
-                if (ground.size() == 1) {
+                if (space) {
                     // We have "-H". Check next
                     if (i + 1 < street.size()) {
                         if (street[i + 1] == '-') {
                             // We have "-H-"
-                            ground.pop();
+                            space = false;
                         }
                         else {
                             // We have "-HH"
                             res++;
-                            houses.pop();
-                            ground.pop();
+                            house = space = false;
                         }
                     }
                     else {
                         // We have "-H" at the end of string
                         res++;
-                        houses.pop();
-                        ground.pop();
+                        house = space = false;
                     }
                 }
             }
             else {
                 // We had already "H-"
-                if (houses.size() == 1 && ground.size() == 1) {
+                if (house && space) {
                     res++;
-                    houses.pop();
-                    ground.pop();
+                    house = space = false;
                 }
                 else {
                     return -1;
@@ -49,32 +46,29 @@ int solution(const string& street) {
             }
         }
         else {
-            if (ground.empty()) {
-                ground.push(street[i]);
+            if (!space) {
+                space = true;
 
-                if (houses.size() == 1) {
+                if (house) {
                     // We had already "H-". Check next
                     if (i + 1 < street.size()) {
                         if (street[i + 1] == 'H') {
                             // We have the case "H-H"
                             res++;
                             i++;
-                            houses.pop();
-                            ground.pop();
+                            house = space = false;
                         }
                         else if (street[i + 1] == '-') {
                             // We have the case "H--"
                             res++;
-                            houses.pop();
-                            ground.pop();
+                            house = space = false;
                         }
                     }
                     else
                     {
                         // We have "H-" at the end of string
                         res++;
-                        houses.pop();
-                        ground.pop();
+                        house = space = false;
                     }
                 }
             }
@@ -82,8 +76,13 @@ int solution(const string& street) {
         i++;
     }
 
-    if (!houses.empty()) {
+    if (house) {
         return -1;
+    }
+    else if (!res)
+    {
+        // All spaces, threat as no error!
+        return 0;
     }
 
     return res > 0 ? res : -1;
@@ -91,13 +90,27 @@ int solution(const string& street) {
 
 int main()
 {
-    string street = "-HH--H-H--H--";
-    int res = solution(street);
-    if (res > 0) {
-        cout << "Needed watertank(s): " << res << endl;
+    vector<string> streets = {"-HH--H-H--H--",
+                             "H-H-H-H-H-H",
+                             "H-H-H-H-H",
+                             "H-HH-HH-H",
+                             "H-HH-H-H-H",
+                             "H-H-HH-HH-H-H",
+                             "H-HH",
+                             "---------",
+                             "----H-----",
+                             "----H-H----",
+                             "----H-HH----"};
+
+    for( const auto& street : streets) {
+        int res = solution(street);
+        if (res >= 0) {
+            cout << "Needed watertank(s): " << res << endl;
+        }
+        else {
+            cout << "Not possible to supply all houses on the street!" << endl;
+        }
     }
-    else {
-        cout << "Not possible to supply all houses on the street!" << endl;
-    }
+
     return 0;
 }
